90c3beedd9ae807bbd71d472762fe9e429ab4fce
Alan Ryan <alan@codecrunchers.ie>
2021-03-03 23:03:12 +0000

    better regexp

--- neo4j/seed_database.sh
+++ neo4j/seed_database.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-container=$(docker  ps | grep -o "family-tree-build_neo4j_[0-9]")
+container=$(docker  ps | grep -o "family-tree-build.*_neo4j_[0-9]")
 cat cypher/family.cypher | sudo  docker exec --interactive  "$container"  cypher-shell -u neo4j -p password
 
 
