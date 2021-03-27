ac01dcdd8328e769b08dcb5ebf1edd2af4720b52
Alan <codecrunchers@users.noreply.github.com>
2021-03-27 00:51:48 +0000

    per repo builds

--- build.sh
+++ build.sh
@@ -2,29 +2,40 @@
 display_usage() {
 	echo "$0 version_for_frontend version_for_backend"
 	echo -e "\nUsage: $0 [arguments] \n"
-	}
+    }
 # if less than two arguments supplied, display usage
-	if [  $# -le 3 ]
+	if [  $# -le 1 ]
 	then
 		display_usage
 		exit 1
 	fi
 
-ftfe_build="$1"
-ftbe_release="$2"
+app_to_release="$1"
+ver="$2"
 
-#tmp_dir=$(mktemp -d -t ft-XXXXXXX)
-#echo "Using Dir $tmp_dir"
-#cd $tmp_dir
-#curl -L https://github.com/codecrunchers/family-tree-build/archive/master.zip --output ft.zip
-#unzip ft.zip
-#cd family-tree-build-master/
-docker-compose build --build-arg FTFE_RELEASE="$ftfe_build" --build-arg FTBE_RELEASE="$ftbe_release"
+if [ "$app_to_release" = "family-tree-frontend" ]; then 
+    ftfe_release=$ver
+elif [ "$app_to_release" = "family-tree-backend" ]; then 
+    ftbe_release=$ver
+fi
+
+
+# If no version specified read from last
+if [ -z "$ftbe_release" ]; then
+    ftbe_release="$(head -n1 ftbe.rel.ver)"
+else
+    echo "$ftbe_release" > ftbe.rel.ver
+fi
+
+# If no version specified read from last
+if [ -z "$ftfe_release" ]; then
+    ftbe_release="$(head -n1 ftfe.rel.ver)"
+else
+    echo "$ftfe_release" > ftfe.rel.ver
+fi
+
+docker-compose build --no-cache --build-arg FTFE_RELEASE="$ftfe_release" --build-arg FTBE_RELEASE="$ftbe_release"
 docker-compose up -d
 sleep 60
 cd neo4j
 ./seed_database.sh
-
-
-
-
